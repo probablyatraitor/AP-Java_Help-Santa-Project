5f75fcad253539d1c9b8418c7f5fe5da725fe8f4
probablyatraitor <38697067+probablyatraitor@users.noreply.github.com>
2018-12-11 12:25:28 -0500

    Add files via upload

--- kids.java
+++ kids.java
@@ -6,11 +6,38 @@ public class kids {
 	public static String names[];
 	public static boolean nice[];
 	public static int age[];
-	public kids () {
-		s = new Scanner("kids.txt");
-		s.useDelimiter(",");
-		while (s.hasNext()) {
 
+
+	public void kids () {
+		s = new Scanner("kids.txt");
+		s.useDelimiter(", ");
+		int x = 0, y = 0;
+		while (s.hasNext()) {
+			y++;
+			if (y == 1) {
+				names[x] = s.nextLine(); //Collect name
+			}
+			if (y == 2) {
+				if (s.nextLine() == "nice") { //Collect nice
+				nice[x] = true;
+				} else nice[x] = false;
+			}
+			if (y == 3) {
+				age[x] = s.nextInt(); //Collect age
+				y = 0;
+				x++; //Next Index
+			}
 		}
+		s.close();
+	}
+
+	public String getName(int i) {
+		return names[i];
+	}
+	public boolean getNice(int i) {
+		return nice[i];
+	}
+	public int getAge(int i) {
+		return age[i];
 	}
 }
