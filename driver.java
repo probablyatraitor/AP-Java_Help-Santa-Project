5016ef8bc7c1c4d7936b81bf52dd3f587a1c2b19
Brian Kiss <45636496+Dexter1946@users.noreply.github.com>
2018-12-13 12:29:45 -0500

    System outs and user input

--- driver.java
+++ driver.java
@@ -1,12 +1,21 @@
 import java.io.*;
 import java.util.*;
 
-public class driver {
-    public static void main(String[] args)throws Exception {
-		Scanner sKids = new Scanner("kids.txt");
-		Scanner sGifts = new Scanner("gifts.txt");
-		Scanner user = new Scanner(System.in);
-
-
+public class driver
+{
+    public static void main(String[] args)throws Exception
+    {
+	Scanner sKids = new Scanner("kids.txt");
+	Scanner sGifts = new Scanner("gifts.txt");
+	Scanner user = new Scanner(System.in);
+	    
+	    
+	System.out.println ("Enter the amount of money");
+	double money = user.nextDouble();
+	System.out.println ("Enter the amount of time (in years)");
+	int years = user.nextInt();
+	    
+	    
+	
     }
 }
