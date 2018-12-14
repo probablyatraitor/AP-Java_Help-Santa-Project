bc85afe7eda289e66005d157a55b12e09f43fcbf
probablyatraitor <38697067+probablyatraitor@users.noreply.github.com>
2018-12-14 12:16:53 -0500

    Updated txt

--- kids.java
+++ kids.java
@@ -2,42 +2,42 @@ import java.io.*;
 import java.util.*;
 
 public class kids {
-	Scanner s;
-	public static String names[];
-	public static boolean nice[];
-	public static int age[];
+	public static ArrayList<String> names;
+	public static ArrayList<Boolean> nice;
+	public static ArrayList<Integer> age;
 
+	public void kids() {
 
-	public void kids () {
-		s = new Scanner("kids.txt");
-		s.useDelimiter(", ");
+	}
+
+	public void importList(ArrayList full) {
+
 		int x = 0, y = 0;
-		while (s.hasNext()) {
+		while (x < full.size()) {
 			y++;
 			if (y == 1) {
-				names[x] = s.nextLine(); //Collect name
+				names.add(full.get(x)); //Collect name
 			}
 			if (y == 2) {
-				if (s.nextLine() == "nice") { //Collect nice
-				nice[x] = true;
-				} else nice[x] = false;
+				if (full.get(x) == "nice") { //Collect nice
+				nice.add(true);
+				} else nice.add(false);
 			}
 			if (y == 3) {
-				age[x] = s.nextInt(); //Collect age
+				age.add(full.get(x)); //Collect age
 				y = 0;
 				x++; //Next Index
 			}
 		}
-		s.close();
 	}
 
 	public String getName(int i) {
-		return names[i];
+		return names.get(i);
 	}
 	public boolean getNice(int i) {
-		return nice[i];
+		return nice.get(i);
 	}
 	public int getAge(int i) {
-		return age[i];
+		return age.get(i);
 	}
 }
